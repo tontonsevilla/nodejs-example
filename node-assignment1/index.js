0343847bb791f03c87e0c7c78f6d8c3d626d7e03
Winston Sevilla <wsevilla@blastasia.com>
2020-10-09 19:20:38 +0800

    Initial Assignment 1

---
+++ node-assignment1/index.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+     http = require('http');
+const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
+
+// CREATE SERVER
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
