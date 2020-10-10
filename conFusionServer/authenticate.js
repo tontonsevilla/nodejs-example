805003513d29359d13bfda4214fad4acf27b88ba
Winston Sevilla <wsevilla@blastasia.com>
2020-10-10 18:12:40 +0800

    Passport Facebook

--- conFusionServer/authenticate.js
+++ conFusionServer/authenticate.js
@@ -3,13 +3,14 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var User = require('./models/user');
 
-// JSON Web Token Require
+// JSON Web Token Require, Facebook Login
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config.js');
 const { use } = require('passport');
 const { Unauthorized } = require('http-errors');
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 // Passport Setup
 passport.use(new LocalStrategy(User.authenticate()));
@@ -27,20 +28,47 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(new JwtStrategy(opts,
-    (jwt_payload, done) => {
-        console.log("JWT payload: ", jwt_payload);
-        User.findOne({_id: jwt_payload._id}, (err, user) => {
+(jwt_payload, done) => {
+    console.log("JWT payload: ", jwt_payload);
+    User.findOne({_id: jwt_payload._id}, (err, user) => {
+        if (err) {
+            return done(err, false);
+        }
+        else if (user) {
+            return done(null, user);
+        }
+        else {
+            return done(null, false);
+        }
+    });
+}));
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
             if (err) {
                 return done(err, false);
             }
-            else if (user) {
+            if (!err && user !== null) {
                 return done(null, user);
             }
             else {
-                return done(null, false);
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                })
             }
         });
-    }));
+    }
+));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 exports.verifyAdmin = (req, res, next) => {
--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '764053200841861',
+        clientSecret: 'b7cf811ee103e1696e785e3d41c534e4'
+    }
 }
