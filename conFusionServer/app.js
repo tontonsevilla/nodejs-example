dd98868914ded0f4d90c14e602ad17a2ae5eb676
Winston Sevilla <wsevilla@blastasia.com>
2020-10-10 10:30:25 +0800

    Basic Authentication

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -18,9 +18,35 @@ app.all('*', (req, res, next) => {
     return next();
   }
   else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('port') + req.url);
   }
 });
+
+function auth (req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+      return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == 'admin' && pass == 'password') {
+      next(); // authorized
+  } else {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');      
+      err.status = 401;
+      next(err);
+  }
+}
+
+app.use(auth);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
