7ca7996f93ebd82b9dff1996ca0778c81304aa69
Winston Sevilla <wsevilla@blastasia.com>
2020-10-10 18:19:25 +0800

    Update

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl': 'mongodb://localhost:27017/conFusion',
     'facebook': {
-        clientId: '764053200841861',
-        clientSecret: 'b7cf811ee103e1696e785e3d41c534e4'
+        clientId: 'APPID',
+        clientSecret: 'SECRET'
     }
 }
